5c10177e7c682b50c9b33560694cd8d200438d19
ai-tdd-labs <aitddlabs@gmail.com>
2026-02-10 23:05:26 +0100

    tests: strengthen dvd fixtures to kill mutants

--- tests/sdk/dvd/dvd_convert_path_to_entrynum/dol/mp4/dvd_convert_path_to_entrynum_mp4_datadir_table_001/dvd_convert_path_to_entrynum_mp4_datadir_table_001.c
+++ tests/sdk/dvd/dvd_convert_path_to_entrynum/dol/mp4/dvd_convert_path_to_entrynum_mp4_datadir_table_001/dvd_convert_path_to_entrynum_mp4_datadir_table_001.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <ogc/cache.h>
+
 typedef uint32_t u32;
 typedef int32_t s32;
 
@@ -22,16 +24,31 @@ static volatile u32 *OUT = (u32 *)0x80300000u;
 
 int main(void) {
     u32 fails = 0;
+    u32 checksum = 2166136261u;
+    s32 r_first = -1;
+    s32 r_last = -1;
+    for (u32 i = 0; i < 16; i++) OUT[i] = 0;
     OUT[0] = 0xDEADBEEFu;
     OUT[1] = (u32)k_mp4_datadir_paths_count;
 
     for (u32 i = 0; i < (u32)k_mp4_datadir_paths_count; i++) {
         s32 r = DVDConvertPathToEntrynum((char *)k_mp4_datadir_paths[i]);
         if (r == -1) fails++;
+        if (i == 0) r_first = r;
+        if (i + 1 == (u32)k_mp4_datadir_paths_count) r_last = r;
+        checksum ^= (u32)r + 0x9e3779b9u + i;
+        checksum *= 16777619u;
     }
 
     OUT[2] = fails;
     OUT[3] = (u32)DVDConvertPathToEntrynum("data/does_not_exist.bin");
+    OUT[4] = (u32)r_first;
+    OUT[5] = (u32)r_last;
+    OUT[6] = checksum;
+    OUT[7] = checksum ^ 0xA5A5A5A5u;
+
+    // Make the debugger's RAM reads deterministic.
+    DCFlushRange((void *)0x80300000u, 0x40);
 
     for (;;) {}
     return 0;
--- tests/sdk/dvd/dvd_convert_path_to_entrynum/host/dvd_convert_path_to_entrynum_mp4_datadir_table_001_scenario.c
+++ tests/sdk/dvd/dvd_convert_path_to_entrynum/host/dvd_convert_path_to_entrynum_mp4_datadir_table_001_scenario.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string.h>
 
 #include "harness/gc_host_ram.h"
 #include "harness/gc_host_scenario.h"
@@ -18,17 +19,29 @@ const char *gc_scenario_out_path(void) { return "../actual/dvd_convert_path_to_e
 void gc_scenario_run(GcRam *ram) {
     gc_dvd_test_set_paths(k_mp4_datadir_paths, (s32)k_mp4_datadir_paths_count);
 
-    uint8_t *p = gc_ram_ptr(ram, 0x80300000u, 16);
+    uint8_t *p = gc_ram_ptr(ram, 0x80300000u, 0x40);
     if (!p) die("gc_ram_ptr failed");
+    memset(p, 0, 0x40);
 
     uint32_t fails = 0;
+    uint32_t checksum = 2166136261u;
+    s32 r_first = -1;
+    s32 r_last = -1;
     for (uint32_t i = 0; i < (uint32_t)k_mp4_datadir_paths_count; i++) {
         s32 r = DVDConvertPathToEntrynum((char *)k_mp4_datadir_paths[i]);
         if (r == -1) fails++;
+        if (i == 0) r_first = r;
+        if (i + 1 == (uint32_t)k_mp4_datadir_paths_count) r_last = r;
+        checksum ^= (uint32_t)r + 0x9e3779b9u + i;
+        checksum *= 16777619u;
     }
 
     wr32be(p + 0, 0xDEADBEEFu);
     wr32be(p + 4, (uint32_t)k_mp4_datadir_paths_count);
     wr32be(p + 8, fails);
     wr32be(p + 12, (uint32_t)DVDConvertPathToEntrynum("data/does_not_exist.bin"));
+    wr32be(p + 16, (uint32_t)r_first);
+    wr32be(p + 20, (uint32_t)r_last);
+    wr32be(p + 24, checksum);
+    wr32be(p + 28, checksum ^ 0xA5A5A5A5u);
 }
